c5146c1f377c6aa2b1b9ea02d3d5c68c85b7b591
Yongjae Yi <dididy@knou.ac.kr>
2019-04-09 21:39:15 +0900

    Add commnet to unusable code

--- DC02_04_201704147_Yongjae_Yi.py
+++ DC02_04_201704147_Yongjae_Yi.py
@@ -83,37 +83,37 @@ def is_it_student_num(byte_stream_string): # This function can make distinction 
         display("Plz include student ID")
 
 
-def stereo_to_mono(input_file, output_file):
-    inp = wave.open(input_file, 'r')
-    params = list(inp.getparams())
-    params[0] = 1 # nchannels
-    params[3] = 0 # nframes
+# def stereo_to_mono(input_file, output_file):
+#     inp = wave.open(input_file, 'r')
+#     params = list(inp.getparams())
+#     params[0] = 1 # nchannels
+#     params[3] = 0 # nframes
 
-    out = wave.open(output_file, 'w')
-    out.setparams(tuple(params))
+#     out = wave.open(output_file, 'w')
+#     out.setparams(tuple(params))
 
-    frame_rate = inp.getframerate()
-    frames = inp.readframes(inp.getnframes())
-    data = np.fromstring(frames, dtype=np.int16)
-    left = data[0::2]
-    out.writeframes(left.tostring())
+#     frame_rate = inp.getframerate()
+#     frames = inp.readframes(inp.getnframes())
+#     data = np.fromstring(frames, dtype=np.int16)
+#     left = data[0::2]
+#     out.writeframes(left.tostring())
 
-    inp.close()
-    out.close()
+#     inp.close()
+#     out.close()
 
-def yield_chunks(input_file, interval):
-    wav = wave.open(input_file)
-    frame_rate = wav.getframerate()
+# def yield_chunks(input_file, interval):
+#     wav = wave.open(input_file)
+#     frame_rate = wav.getframerate()
 
-    chunk_size = int(round(frame_rate * interval))
-    total_size = wav.getnframes()
+#     chunk_size = int(round(frame_rate * interval))
+#     total_size = wav.getnframes()
 
-    while True:
-        chunk = wav.readframes(chunk_size)
-        if len(chunk) == 0:
-            return
+#     while True:
+#         chunk = wav.readframes(chunk_size)
+#         if len(chunk) == 0:
+#             return
 
-        yield frame_rate, np.fromstring(chunk, dtype=np.int16)
+#         yield frame_rate, np.fromstring(chunk, dtype=np.int16)
 
 def dominant(frame_rate, chunk):
     w = np.fft.fft(chunk)
@@ -157,21 +157,21 @@ def decode_bitchunks(chunk_bits, chunks):
 
     return out_bytes
 
-def decode_file(input_file, speed):
-    wav = wave.open(input_file)
-    if wav.getnchannels() == 2:
-        mono = StringIO()
-        stereo_to_mono(input_file, mono)
+# def decode_file(input_file, speed):
+#     wav = wave.open(input_file)
+#     if wav.getnchannels() == 2:
+#         mono = StringIO()
+#         stereo_to_mono(input_file, mono)
 
-        mono.seek(0)
-        input_file = mono
-    wav.close()
+#         mono.seek(0)
+#         input_file = mono
+#     wav.close()
 
-    offset = 0
-    for frame_rate, chunk in yield_chunks(input_file, speed / 2):
-        dom = dominant(frame_rate, chunk)
-        print("{} => {}".format(offset, dom))
-        offset += 1
+#     offset = 0
+#     for frame_rate, chunk in yield_chunks(input_file, speed / 2):
+#         dom = dominant(frame_rate, chunk)
+#         print("{} => {}".format(offset, dom))
+#         offset += 1
 
 def extract_packet(freqs):
     freqs = freqs[::2]
